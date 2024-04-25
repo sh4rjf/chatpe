353022bd9f10dce30aa8b6a9eddb038b4f90755f
Omar Dolores <omar.dolores@elcades.pe>
2024-04-24 22:50:39 -0500

    Add files via upload

--- includes/chatpe-functions.php
+++ includes/chatpe-functions.php
@@ -1,12 +1,40 @@
 <?php
 // Función para agregar el botón de ChatPe en el contenido del sitio
+// Función para agregar el botón de ChatPe en el contenido del sitio
 function agregar_boton_chatpe_al_contenido($content) {
-    if (is_singular()) {
-        $content .= mostrar_boton_chatpe();
+    // Verificar si estamos en una página individual y si el contenido no está vacío
+    if (is_singular() && !empty($content)) {
+        // Agregar el botón de ChatPe después del primer párrafo del contenido
+        $content = insertar_boton_chatpe_despues_del_primer_parrafo($content);
     }
     return $content;
 }
+
+// Función para insertar el botón de ChatPe después del primer párrafo del contenido
+function insertar_boton_chatpe_despues_del_primer_parrafo($content) {
+    // Definir el marcador de posición para buscar el primer párrafo
+    $primer_parrafo = '<p>';
+    
+    // Encontrar la posición del primer párrafo en el contenido
+    $posicion_primer_parrafo = strpos($content, $primer_parrafo);
+    
+    // Verificar si se encontró el primer párrafo
+    if ($posicion_primer_parrafo !== false) {
+        // Separar el contenido en dos partes: antes y después del primer párrafo
+        $parte_anterior = substr($content, 0, $posicion_primer_parrafo + strlen($primer_parrafo));
+        $parte_posterior = substr($content, $posicion_primer_parrafo + strlen($primer_parrafo));
+        
+        // Agregar el botón de ChatPe después del primer párrafo
+        $content = $parte_anterior . mostrar_boton_chatpe() . $parte_posterior;
+    }
+    
+    return $content;
+}
+
+
 add_filter('the_content', 'agregar_boton_chatpe_al_contenido');
+
+
 
 // Función para mostrar el botón de ChatPe
 function mostrar_boton_chatpe() {
@@ -28,6 +56,7 @@ function mostrar_boton_chatpe() {
     </div>
     <?php
 }
+
 
 // JavaScript para manejar la selección de la imagen
 function chatpe_seleccionar_imagen() { ?>
@@ -57,3 +86,4 @@ function chatpe_seleccionar_imagen() { ?>
 <?php }
 add_action('wp_footer', 'mostrar_boton_chatpe');
 ?>
+
