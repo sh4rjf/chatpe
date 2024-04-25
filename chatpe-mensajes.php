7ee0097fad480ece87b067782e46e35f6baddaa3
Omar Dolores <omar.dolores@elcades.pe>
2024-04-24 22:16:42 -0500

    Add files via upload

---
+++ chatpe-mensajes.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Plugin Name: ChatPe Mensajes 
+Plugin URI: https://github.com/sh4rjf/chatpe
+Description: Es una solución que incorpora un sistema de chat en tiempo real en tu sitio web, facilitando la interacción instantánea entre los visitantes.
+Version: 1.0
+Author: Omar Dolores
+Author URI: https://github.com/sh4rjf/chatpe
+License: GPLv3 or later
+License URI: https://www.gnu.org/licenses/gpl-3.0.html
+*/
+
+require_once plugin_dir_path(__FILE__) . 'includes/chatpe-functions.php';
+require_once plugin_dir_path(__FILE__) . 'admin/chatpe-admin.php';
+?>
